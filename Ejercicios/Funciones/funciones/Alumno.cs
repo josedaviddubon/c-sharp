f5a89c31b750f31e256844fe050e430b0973273b
Jose Dubon <josedubon17@hotmail.com>
2022-07-12 15:51:58 -0600

    Funciones para la clase Alumno

--- Ejercicios/Funciones/funciones/Alumno.cs
+++ Ejercicios/Funciones/funciones/Alumno.cs
@@ -6,15 +6,34 @@ public class Alumno
 
     public string SegundoNombre { get; set; }
 
+    public bool Activo { get; set; }
+
     public Alumno(int id, string primerNombre, string segungoNombre)
     {
         Id = id;
         PrimerNombre = primerNombre;
         SegundoNombre = segungoNombre;
+        Activo = true;
+
     }
 
     public string nombreCompleto()
     {
+        if(Activo == true){
         return PrimerNombre + " " + SegundoNombre;
+        }
+        return "Alumno Inactivo";
+    }
+
+
+    public void inactivarAlumno()
+    {
+        Activo = false;
+    }
+
+    
+    public void activarAlumno()
+    {
+        Activo = true;
     }
 }
--- Ejercicios/Funciones/funciones/Program.cs
+++ Ejercicios/Funciones/funciones/Program.cs
@@ -7,7 +7,19 @@ namespace Funciones  // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             Alumno a  = new Alumno(1, "Juan", "Perez"); 
+            Alumno b  = new Alumno(2, "Maria", "Martinez");
+
+            a.inactivarAlumno();
+
             Console.WriteLine(a.nombreCompleto());
+            Console.WriteLine(b.nombreCompleto());
+
+            Console.WriteLine("");
+
+            a.activarAlumno();
+            Console.WriteLine(a.nombreCompleto());
+            Console.WriteLine(b.nombreCompleto());
+
         }
     }
 }
