07ab5ad1b2fe338b29b469791e90ddb1e667b3d3
Jose Dubon <josedubon17@hotmail.com>
2022-07-12 15:19:54 -0600

    Ejemplo Propiedades en Carpeta Funciones

---
+++ Ejercicios/Funciones/funciones/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Funciones  // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a  = new Alumno(1, "Juan", "Perez"); 
+            Console.WriteLine(a.nombreCompleto());
+        }
+    }
+}
